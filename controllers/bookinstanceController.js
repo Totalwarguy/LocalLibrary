59c9435fdc9e43a134ab2ad5d27c5e7b554fff86
Lab User <LabUser@labs.salina.k-state.edu>
2018-10-18 09:01:58 -0500

    Implemented Update to "Genre, Book, and Author" models

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -97,13 +97,52 @@ exports.bookinstance_create_post = [
 ];
 
 // Display BookInstance delete form on GET.
-exports.bookinstance_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance delete GET');
+exports.bookinstance_delete_get = function(req, res, next) {
+	
+    async.parallel({
+		bookinstance: function(callback) {
+			BookInstance.findById(req.params.id)
+			.populate('book')
+			.exec(callback);
+		}
+    }, function(err, results) {
+		if (err) { return next(err); }
+
+		if (results.bookinstance == null) {
+			res.redirect('/catalog/bookinstances');
+		}
+
+		res.render('bookinstance_delete', {title: 'Delete Book Instance', bookinstance: results.bookinstance});
+
+    });
+	
 };
 
 // Handle BookInstance delete on POST.
-exports.bookinstance_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance delete POST');
+exports.bookinstance_delete_post = function(req, res, next) {
+	
+    const bookinstances_url = '/catalog/bookinstances';
+	async.parallel({
+		bookinstance: function(callback) {
+			BookInstance.findById(req.body.bookinstanceid)
+			.exec(callback);
+		}
+	}, function(err, results) {
+		if (err) { return next(err); }
+
+		if (results.bookinstance == null) {
+			res.redirect(bookinstances_url);
+			return;
+		} else {
+			BookInstance.findByIdAndRemove(req.body.bookinstanceid, function(err) {
+				if (err) { return next(err); }
+
+				res.redirect(bookinstances_url);
+			});
+		}
+
+	});
+
 };
 
 // Display BookInstance update form on GET.
